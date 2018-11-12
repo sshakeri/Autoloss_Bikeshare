79a92290010f5a92885f1d3c80bc1e92e32e2ac4
Shahin Shakeri <shain.tb.shakeri@gmail.com>
2018-11-12 10:02:44 +0000

    Updates

---
+++ RegressionNotebook.R
@@ -0,0 +1,48 @@
+## ------------------------------------------------------------------------
+library('lattice')
+Autoloss <- read.csv(url("https://drive.google.com/uc?export=download&id=1cbk_KGdo5sfY0c4_ALB5ULTeYQ87uU6k"),na.strings = '?')
+Autoloss <-na.omit (Autoloss)
+ByDoors=tapply(Autoloss$Losses, Autoloss$NumDoors, mean)
+barplot(ByDoors, col = rainbow(15),xlab="Number of Doors",ylab = "Loss",ylim=c(0,150))
+
+
+## ------------------------------------------------------------------------
+Lowest<- Autoloss[order(Autoloss$Losses,decreasing=FALSE),][1:10,]
+Lowest<-tapply(Lowest$Losses,Lowest$BodyStyle , mean)
+Lowest
+
+
+## ------------------------------------------------------------------------
+ByWheels<-tapply(Autoloss$Losses,Autoloss$BodyStyle,mean)
+ByWheels
+boxplot(Losses~BodyStyle,data=Autoloss ,col = rainbow(15))
+
+
+## ------------------------------------------------------------------------
+xyplot(Losses~Price,data=Autoloss, col = rainbow(50))
+
+## ------------------------------------------------------------------------
+Bikeshare <- read.csv(url("https://drive.google.com/uc?export=download&id=1jtfh-qmyDM3l_nU3AkPP_ZsepL-jHW8I"),na.strings = '?')
+LModel=lm(Rentals~Temperature,data=Bikeshare)
+{plot(Bikeshare$Temperature,Bikeshare$Rentals,col='blue') 
+abline(LModel,col="red" )}
+
+## ------------------------------------------------------------------------
+summary(LModel)
+
+## ------------------------------------------------------------------------
+LModel=lm(Rentals~Humidity,data=Bikeshare)
+{plot(Bikeshare$Humidity,Bikeshare$Rentals,col='blue') 
+abline(LModel,col="red" )}
+
+## ------------------------------------------------------------------------
+summary(LModel)
+
+## ------------------------------------------------------------------------
+LModel=lm(Rentals~Windspeed,data=Bikeshare)
+{plot(Bikeshare$Windspeed,Bikeshare$Rentals,col='blue') 
+abline(LModel,col="red" )}
+
+## ------------------------------------------------------------------------
+summary(LModel)
+
---
+++ convertor.R
@@ -0,0 +1,3 @@
+library(knitr)
+purl("RegressionNotebook.Rmd")
+
